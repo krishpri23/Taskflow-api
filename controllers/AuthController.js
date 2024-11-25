ca3ce912fccf6fcdc1ef8cf31a62f535fdee89da
Krishnapriya <17042821+krishpri23@users.noreply.github.com>
2024-11-24 16:21:36 -0800

    changed login controller

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -3,54 +3,25 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
 
-// @desc register, public route, POST
-const register = asyncHandler(async (req, res) => {
-  const { username, password, roles } = req.body;
+// @desc Login
+// @route POST /auth
+// @access Public
+const login = async (req, res) => {
+  const { username, password } = req.body;
 
-  if (!username || !password || !Array.isArray(roles) || !roles.length) {
+  if (!username || !password) {
     return res.status(400).json({ message: "All fields are required" });
   }
 
-  console.log(username, password, "inside create user");
-
-  // check for duplicate, exec() returns a promise, lean returns plain js objects
-  const duplicate = await User.findOne({ username }).lean().exec();
-  console.log("duplicate ", duplicate);
-  if (duplicate) {
-    // 409 - conflict
-    return res.status(409).json({ message: "User already exists" });
-  }
-
-  const hashedPwd = await bcrypt.hash(password, 10);
-  const userObject = { username, password: hashedPwd, roles };
-
-  // create new user
-  const newUser = await User.create(userObject);
-
-  if (newUser) {
-    res.status(201).json({ message: "New User created" });
-  } else {
-    res.status(400).json({ message: "Invalid user data" });
-  }
-});
-
-// @desc login, public route, POST, generates both tokens on login
-const login = asyncHandler(async (req, res) => {
-  const { username, password } = req.body;
-  console.log(req.body);
-
-  if (!username || !password) {
-    return res.status(400).json({ message: "All fields required!" });
-  }
-
   const foundUser = await User.findOne({ username }).exec();
-  const match = await bcrypt.compare(password, foundUser.password);
 
-  if (!foundUser || !foundUser.active || !match) {
+  if (!foundUser || !foundUser.active) {
     return res.status(401).json({ message: "Unauthorized" });
   }
 
-  //jwt.sign(payload, secretOrPrivateKey, [options, callback]);
+  const match = await bcrypt.compare(password, foundUser.password);
+
+  if (!match) return res.status(401).json({ message: "Unauthorized" });
 
   const accessToken = jwt.sign(
     {
@@ -60,29 +31,26 @@ const login = asyncHandler(async (req, res) => {
       },
     },
     process.env.ACCESS_TOKEN_SECRET,
-    {
-      expiresIn: "10m",
-    }
+    { expiresIn: "15m" }
   );
 
   const refreshToken = jwt.sign(
-    {
-      username: foundUser.username,
-    },
+    { username: foundUser.username },
     process.env.REFRESH_TOKEN_SECRET,
-    { expiresIn: "1d" }
+    { expiresIn: "7d" }
   );
 
-  // create secure cookie with refresh token
+  // Create secure cookie with refresh token
   res.cookie("jwt", refreshToken, {
     httpOnly: true, //accessible only by web server
     secure: true, //https
     sameSite: "None", //cross-site cookie
-    maxAge: 7 * 24 * 60 * 60 * 1000, // cookie expiry match refresh token
+    maxAge: 7 * 24 * 60 * 60 * 1000, //cookie expiry: set to match rT
   });
 
+  // Send accessToken containing username and roles
   res.json({ accessToken });
-});
+};
 
 // access token is expired so we send refresh token to generate a new access token
 const refresh = asyncHandler(async (req, res) => {
